0c1bba57858079e58407de668ffaa17df3ae6513
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-05-29 01:18:49 +0500

    set all the routes for screens based on role

--- lib/classes/route.dart
+++ lib/classes/route.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Router {
-  Widget? icon;
+  final IconData icon;
   final String routeLink;
   final Widget widget;
   final String title;
   List<String>? allowedRoles;
 
   Router({
-    Widget? icon,
+    required this.icon,
     required this.title,
     required this.routeLink,
     required this.widget,
