019c99a0337f4f76c812b4c3b3c1c6f01da8de9d
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-06-26 12:43:04 +0500

    drawer style updated

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:restaurant_management_system/routes/footer_routes.dart';
 import 'package:restaurant_management_system/routes/sidebar_routes.dart';
 import 'package:restaurant_management_system/classes/route.dart' as _MyAppRoute;
+import 'package:restaurant_management_system/services/SharedPref_Helper.dart';
 
 class MyDrawer extends StatefulWidget {
   const MyDrawer({super.key});
@@ -11,40 +11,87 @@ class MyDrawer extends StatefulWidget {
 }
 
 class _MyDrawerState extends State<MyDrawer> {
-
   late List<_MyAppRoute.Router> approute;
+  late SharedPref pref;
+
+  final TextEditingController _userController = TextEditingController();
+  final TextEditingController _roleController = TextEditingController();
+
+  
 
   @override
   void initState() {
     super.initState();
     approute = getRoleBasedRoutes();
+    initiatePreference();
+
+  }
+
+  Future<void> initiatePreference() async {
+    pref = SharedPref();
+    await pref.init();
+    setState(() {
+      _userController.text = pref.getValue("USER") ?? "USER-NAME";
+      _roleController.text = pref.getValue("ROLE") ?? "USER-ROLE";
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
         children: [
-          const DrawerHeader(
+          UserAccountsDrawerHeader(
             decoration: BoxDecoration(
               color: Colors.blueAccent,
             ),
-            child: Text(
-              'My side bar',
-              style: TextStyle(color: Colors.white, fontSize: 24),
+            accountName: Text(
+              _userController.text,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            accountEmail: Text(_roleController.text),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.person,
+                size: 40,
+                color: Colors.blueAccent,
+              ),
             ),
           ),
-          for (var item in approute) 
-            ListTile(
-              leading: Icon(item.icon),
-              title: Text(
-                item.title,
-                style: const TextStyle(color: Colors.black),
-              ),
-              onTap: () {
-                Navigator.pushNamed(context, item.routeLink);
-              },
+          Expanded(
+            child: ListView(
+              children: [
+                for (var item in approute)
+                  ListTile(
+                    leading: Icon(item.icon),
+                    title: Text(
+                      item.title,
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(context, item.routeLink);
+                    },
+                  ),
+                Divider(),
+              ],
             ),
+          ),
+          // ListTile(
+          //   leading: Icon(Icons.settings),
+          //   title: Text('Settings'),
+          //   onTap: () {
+          //     Navigator.pushNamed(context, '/settings');
+          //   },
+          // ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              // Add logout functionality here
+              Navigator.pushNamed(context, '/logout');
+            },
+          ),
         ],
       ),
     );
